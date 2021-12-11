9e8e707665248f00b9bd4cd692d3902be35bc3e8
balaitous <balaitous@mailoo.org>
2021-12-11 13:51:35 +0100

    avant de tester std::optional

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,158 +1,56 @@
-#include <filesystem>
-#include <list>
-#include <map>
+#include <fstream>
+#include <iostream>
+#include <regex>
 #include <stdexcept>
-#include <string>
+
+#include "fmt/format.h"
+
+#include "parser.h"
 
 
-using string_t = std::string;
+const std::regex parser_t::group_regex {R"(^\s*\[([_a-zA-Z]\w*)\]\s*$)"};
+const std::regex parser_t::comment_regex {R"(^\s*[;\#](.*)$)"};
+const std::regex parser_t::option_regex {
+    R"(^\s*([_a-zA-Z]\w*)(\[([a-zA-Z]\w*)\])?\s*=\s*(.*)$)" };
+const std::regex parser_t::empty_regex {R"(^\s*$)"};
 
-
-class value_t {
-
-    using data_t = std::map<string_t, string_t>;
-
-public:
-    value_t() : data {{"", ""}} {};
-
-    const string_t& get(string_t conf_name)
-    {
-        while (true) {
-            const auto it = data.find(conf_name);
-            if (it != data.end()) {
-                return data.at(conf_name);
+void parser_t::load_file(const std::filesystem::path& filename)
+{
+    std::cout << "parser_t::load_file " << filename << std::endl;
+    std::ifstream is {filename};
+    std::string line;
+    int lineno = 0;
+    std::string group { "default" };
+    std::smatch m;
+    while (getline(is, line)) {
+        lineno++;
+        std::cout << "Line: " << line << std::endl;
+        if (std::regex_match(line, empty_regex)) {
+            continue;
+        }
+        if (std::regex_match(line, m, group_regex)) {
+            std::cout << "Group: " << m[1] << std::endl;
+            group = m[1];
+            continue;
+        }
+        if (std::regex_match(line, m, comment_regex)) {
+            std::cout << "Comment: " << m[1] << std::endl;
+            continue;
+        }
+        if (std::regex_match(line, m, option_regex)) {
+            std::cout << "Option: key=" << m[1] << ", config=" << m[3]
+                      << ", value=" << m[4] << std::endl;
+            option_metadata_t::ptr option;
+            option_key_t key { group, m[1] };
+            try {
+                option = options.at(key);
             }
-            if (conf_name.empty()) {
-                throw std::out_of_range("Not found");
+            catch (std::out_of_range&) {
+                options[key] = option = std::make_shared<option_metadata_t>();
             }
-            int pos = conf_name.find_last_of("_");
-            if (pos == std::string::npos) {
-                conf_name.erase();
-            } else {
-                conf_name.erase(pos);
-            }
+            option->add_value(m[3], m[4]);
+            continue;
         }
+        throw std::runtime_error(fmt::format("Error at {}:{}:\n\t{}\n", filename.native(), lineno, line));
     }
-
-    ~value_t() {};
-
-private:
-    data_t data;
-
-};
-
-
-using paths_t = std::list<std::filesystem::path>;
-
-
-class node_t
-{
-    using node_map_t = std::map<string_t, node_t>;
-
-    enum value_t {null, node_map, node_leaf};
-    union node_value_t
-    {
-        nullptr_t *node_null;
-        node_map_t *node_map;
-        string_t *node_leaf;
-
-        node_value_t()
-        {
-            node_null = nullptr;
-        }
-    };
-
-
-public:
-
-    node_t() : node_t(nullptr) {};
-
-    node_t(node_t *parent)
-        : node_type(value_t::null)
-        , node_value()
-        , parent(parent)
-    {};
-
-    node_t& operator[](const string_t& name)
-    {
-        switch (node_type)
-        {
-        case value_t::null:
-            node_value.node_map = new node_map_t();
-            break;
-        case value_t::node_leaf:
-            throw std::out_of_range("Leaf !!!");
-        default:
-            break;
-        }
-        if (node_value.node_map->find(name) == node_value.node_map->end()) {
-        } else {
-            return (*node_value.node_map)[name];
-        }
-    }
-
-    // paths_t get_
-
-
-
-private:
-    value_t node_type;
-    node_value_t node_value;
-    node_t *parent;
-
-    void parse_file(const std::list<std::filesystem::path>& files, node_map_t& map) {
-
-    }
-
-};
-
-
-class settings_t
-{
-public:
-    virtual settings_t& operator[](const string_t& key) = 0;
-    virtual ~settings_t() = 0;
-};
-
-
-class settings_node_t : public settings_t
-{
-public:
-    settings_node_t(const std::string& name, settings_node_t* parent)
-        : path(parent ? (parent->path / name) : std::filesystem::path(name))
-    {};
-
-    virtual settings_t& operator[](const string_t& key)
-    {
-        if (data.find(key) != data.end())
-        {
-            return data[key];
-        }
-
-    }
-    virtual ~settings_node_t() {};
-
-public:
-    std::map<string_t, settings_t> data;
-    std::filesystem::path path;
-};
-
-
-class settings_leaf_t : public settings_t
-{
-public:
-    settings_leaf_t(const string_t& data) : data(data) {};
-    virtual const settings_t& operator[](const string_t& key)
-    {
-        throw std::out_of_range("Leaf object");
-    }
-    virtual ~settings_leaf_t() {};
-
-private:
-    string_t data;
-};
-
-
-int main(int argc, char** argv)
-{
 }
---
+++ src/parser.h
@@ -0,0 +1,69 @@
+#include <filesystem>
+#include <memory>
+#include <regex>
+#include <string>
+
+
+class option_metadata_t
+{
+public:
+    typedef std::shared_ptr<option_metadata_t> ptr;
+
+private:
+    typedef std::map<std::string, std::string> config_value_t;
+
+public:
+    option_metadata_t() {};
+
+    void add_value(const std::string& config, const std::string& value) {
+        std::cout << "add_value: config=" << config << ", value=" << value << std::endl;
+        values.emplace(config, value);
+    };
+
+    const std::string& value(const std::string& config) {
+        std::string key = config;
+        while (true) {
+            const auto it = values.find(key);
+            if (it != values.end()) {
+                return values.at(key);
+            }
+            if (key.empty()) {
+                throw std::out_of_range("Not found");
+            }
+            int pos = key.find_last_of("_");
+            if (pos == std::string::npos) {
+                key.erase();
+            } else {
+                key.erase(pos);
+            }
+        }
+    };
+
+private:
+    config_value_t values;
+    std::string doc;
+
+};
+
+
+class parser_t
+{
+public:
+    typedef std::shared_ptr<parser_t> ptr;
+    typedef std::pair<std::string, std::string> option_key_t;
+    typedef std::map<option_key_t, option_metadata_t::ptr> options_t;
+
+    parser_t() {};
+    void load_file(const std::filesystem::path& filename);
+    bool empty() { return options.empty(); }
+    const options_t& get_options() { return options; };
+
+private:
+
+    static const std::regex group_regex;
+    static const std::regex comment_regex;
+    static const std::regex option_regex;
+    static const std::regex empty_regex;
+
+    options_t options;
+};
