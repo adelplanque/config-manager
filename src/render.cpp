c573258b14be114e2c753f05e8b1b9270f8a541f
balaitous <balaitous@mailoo.org>
2021-12-12 18:24:03 +0100

    Ébauche renderer jinja

---
+++ src/render.cpp
@@ -0,0 +1,8 @@
+#include <jinja2cpp/template.h>
+
+#include "render.h"
+
+const std::string& render(const std::string& data)
+{
+
+}
---
+++ src/render.h
@@ -0,0 +1,8 @@
+#ifndef __render_h__
+#define __render_h__
+
+
+const std::string& render(const std::string& data);
+
+
+#endif
